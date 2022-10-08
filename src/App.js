19108f3d30f6de04bbcea429d2622d3897fe5414
Amir Basirati <amirbasirati13@gmail.com>
2022-10-08 16:30:05 +0330

    project initialized

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Notes from "./components/Notes";
+import SearchBar from "./components/SearchBar";
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <SearchBar />
+        <Notes />      
+    </div>
+  );
+}
+
+export default App;
