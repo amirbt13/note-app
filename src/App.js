d0c9ed48075a4e118dc1646e00eae4bbec4ca701
Amir Basirati <amirbasirati13@gmail.com>
2022-10-30 16:39:41 +0330

    responsive updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Notes from "./components/Notes";
 import SearchBar from "./components/SearchBar";
+import './App.css'
 
 
 function App() {
 
   const [notes, setNotes] = useState([])
+  const [darkMode, setDarkMode] = useState(true)
 
   return (
-    <div className="App">
-        <Header />
+    <div className={`${darkMode && "dark-mode"} "App"`} >
+        <Header darkMode={darkMode}setDarkMode={setDarkMode}/>
         <SearchBar notes={notes} setNotes={setNotes}/>
         <Notes notes={notes} setNotes={setNotes}/>      
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './Header.module.css'
 
-const Header = () => {
+const Header = ({ darkMode, setDarkMode }) => {
+
+  const darkHandler = () => {
+    setDarkMode(prevDarkMode => !prevDarkMode)
+  }
+
   return (
     <div className={styles.container}>
-        <h1>Notes</h1>
-        <button>Toggle Mode</button>
+        <h1 className={darkMode ? styles.dark : styles.light}>Notes</h1>
+        <button onClick={darkHandler}>Toggle Mode</button>
     </div>
   )
 }
